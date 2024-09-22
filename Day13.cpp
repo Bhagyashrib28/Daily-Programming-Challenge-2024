e91f7783db7b639e792eabbf13ca575a7bb73c7c
Bhagyashrib28 <bhagyashribelhekar28@gmail.com>
2024-09-22 23:19:45 +0530

    Add files via upload

---
+++ Day13.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <string>
+
+std::string longestPalindrome(const std::string& s) {
+    if (s.empty()) return "";
+    int start = 0, end = 0;
+    auto expandAroundCenter = [&](int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1; 
+    };
+
+    for (int i = 0; i < s.length(); ++i) {
+        int len1 = expandAroundCenter(i, i);
+        int len2 = expandAroundCenter(i, i + 1);
+        int len = std::max(len1, len2);
+
+        if (len > end - start) {
+            start = i - (len - 1) / 2; 
+            end = i + len / 2;       
+        }
+    }
+
+    return s.substr(start, end - start + 1);
+}
+
+int main() {
+    std::string s = "babad";
+    std::string result = longestPalindrome(s);
+    std::cout << "Longest palindromic substring: " << result << std::endl;
+    return 0;
+}
